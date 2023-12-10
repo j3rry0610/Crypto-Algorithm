623b781520faf0347fb69fe3d5f1b703d2a5a5b8
j3rry <qhwang996@gmail.com>
2023-12-10 23:51:12 +0800

    refactor

--- des.h
+++
@@ -1,7 +0,0 @@
-#ifndef DES_H
-#define DES_H
-
-void des_encipher(const unsigned char *plain, unsigned char *cipher, const unsigned char *key);
-void des_decipher(const unsigned char *cipher, unsigned char *plain, const unsigned char *key);
-
-#endif
